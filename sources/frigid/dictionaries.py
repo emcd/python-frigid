1e47b2746f625c5367b30e5f1c6f9904724d17a8
Eric McDonald <emcd@users.noreply.github.com>
2024-12-03 15:40:40 -0800

    Initial population of files. (Coauthor: Anthropic claude-3-5-sonnet).

---
+++ sources/frigid/dictionaries.py
@@ -0,0 +1,193 @@
+# vim: set filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+#============================================================================#
+#                                                                            #
+#  Licensed under the Apache License, Version 2.0 (the "License");           #
+#  you may not use this file except in compliance with the License.          #
+#  You may obtain a copy of the License at                                   #
+#                                                                            #
+#      http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                            #
+#  Unless required by applicable law or agreed to in writing, software       #
+#  distributed under the License is distributed on an "AS IS" BASIS,         #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#  See the License for the specific language governing permissions and       #
+#  limitations under the License.                                            #
+#                                                                            #
+#============================================================================#
+
+
+# pylint: disable=line-too-long
+''' Immutable dictionaries.
+
+    Dictionaries which cannot be modified after creation.
+
+    * :py:class:`AbstractDictionary`:
+      Base class defining the immutable dictionary interface. Implementations
+      must provide ``__getitem__``, ``__iter__``, and ``__len__``.
+
+    * :py:class:`Dictionary`:
+      Standard implementation of an immutable dictionary. Supports all usual
+      dict read operations but prevents any modifications.
+
+    * :py:class:`ValidatorDictionary`:
+      Validates entries before addition using a supplied predicate function.
+
+    >>> from frigid import Dictionary
+    >>> d = Dictionary( x = 1, y = 2 )
+    >>> d[ 'z' ] = 3  # Attempt to add entry
+    Traceback (most recent call last):
+        ...
+    frigid.exceptions.EntryImmutabilityError: Cannot assign or delete entry for 'z'.
+    >>> d[ 'x' ] = 4  # Attempt modification
+    Traceback (most recent call last):
+        ...
+    frigid.exceptions.EntryImmutabilityError: Cannot assign or delete entry for 'x'.
+    >>> del d[ 'y' ]  # Attempt removal
+    Traceback (most recent call last):
+        ...
+    frigid.exceptions.EntryImmutabilityError: Cannot assign or delete entry for 'y'.
+'''
+# pylint: enable=line-too-long
+
+
+from . import __
+from . import objects as _objects
+
+
+class AbstractDictionary( __.cabc.Mapping[ __.H, __.V ] ):
+    ''' Abstract base class for immutable dictionaries.
+
+        An immutable dictionary prevents modification or removal of entries
+        after creation. This provides a clean interface for dictionaries
+        that should never change.
+
+        Implementations must provide:
+        - __getitem__, __iter__, __len__
+    '''
+
+    @__.abstract_member_function
+    def __iter__( self ) -> __.cabc.Iterator[ __.H ]:
+        raise NotImplementedError  # pragma: no coverage
+
+    @__.abstract_member_function
+    def __len__( self ) -> int:
+        raise NotImplementedError  # pragma: no coverage
+
+    @__.abstract_member_function
+    def __getitem__( self, key: __.H ) -> __.V:
+        raise NotImplementedError  # pragma: no coverage
+
+    def __setitem__( self, key: __.H, value: __.V ) -> None:
+        from .exceptions import EntryImmutabilityError
+        raise EntryImmutabilityError( key )
+
+    def __delitem__( self, key: __.H ) -> None:
+        from .exceptions import EntryImmutabilityError
+        raise EntryImmutabilityError( key )
+
+    def __or__( self, other: __.cabc.Mapping[ __.H, __.V ] ) -> __.a.Self:
+        if not isinstance( other, __.cabc.Mapping ): return NotImplemented
+        return type( self )( other )
+
+    def __ror__( self, other: __.cabc.Mapping[ __.H, __.V ] ) -> __.a.Self:
+        if not isinstance( other, __.cabc.Mapping ): return NotImplemented
+        return self | other
+
+    def __and__(
+        self,
+        other: __.cabc.Set[ __.H ] | __.cabc.Mapping[ __.H, __.V ]
+    ) -> __.a.Self:
+        if isinstance( other, __.cabc.Mapping ):
+            return type( self )(
+                ( key, value ) for key, value in self.items( )
+                if key in other and other[ key ] == value )
+        if isinstance( other, ( __.cabc.Set, __.cabc.KeysView ) ):
+            return type( self )(
+                ( key, self[ key ] ) for key in self.keys( ) & other )
+        return NotImplemented
+
+    def __rand__(
+        self,
+        other: __.cabc.Set[ __.H ] | __.cabc.Mapping[ __.H, __.V ]
+    ) -> __.a.Self:
+        if not isinstance(
+            other, ( __.cabc.Mapping, __.cabc.Set, __.cabc.KeysView )
+        ): return NotImplemented
+        return self & other
+
+
+class Dictionary( _objects.Object, AbstractDictionary[ __.H, __.V ] ):
+    ''' Immutable dictionary. '''
+
+    __slots__ = ( '_data_', )
+
+    def __init__(
+        self,
+        *iterables: __.DictionaryPositionalArgument[ __.H, __.V ],
+        **entries: __.DictionaryNominativeArgument[ __.V ],
+    ) -> None:
+        data = __.AccretiveDictionary( *iterables, **entries )
+        super( ).__setattr__( '_data_', __.DictionaryProxy( data ) )
+        super( ).__init__( )
+
+    def __iter__( self ) -> __.cabc.Iterator[ __.H ]:
+        return iter( self._data_ )
+
+    def __len__( self ) -> int:
+        return len( self._data_ )
+
+    def __repr__( self ) -> str:
+        return f"{__.calculate_fqname( self )}( {self._data_!r} )"
+
+    def __str__( self ) -> str:
+        return str( self._data_ )
+
+    def __contains__( self, key: __.a.Any ) -> bool:
+        return key in self._data_
+
+    def __getitem__( self, key: __.H ) -> __.V:
+        return self._data_[ key ]
+
+    def __eq__( self, other: __.a.Any ) -> __.ComparisonResult:
+        if isinstance( other, __.cabc.Mapping ):
+            return self._data_ == other
+        return NotImplemented
+
+    def __ne__( self, other: __.a.Any ) -> __.ComparisonResult:
+        if isinstance( other, __.cabc.Mapping ):
+            return self._data_ != other
+        return NotImplemented
+
+    def get(
+        self, key: __.H, default: __.Optional[ __.V ] = __.absent
+    ) -> __.a.Annotation[
+        __.V,
+        __.a.Doc(
+            'Value of entry, if it exists. '
+            'Else, supplied default value or ``None``.' )
+    ]:
+        ''' Retrieves entry associated with key, if it exists. '''
+        if __.is_absent( default ):
+            return self._data_.get( key )  # type: ignore
+        return self._data_.get( key, default )
+
+    def keys( self ) -> __.cabc.KeysView[ __.H ]:
+        ''' Provides iterable view over dictionary keys. '''
+        return self._data_.keys( )
+
+    def items( self ) -> __.cabc.ItemsView[ __.H, __.V ]:
+        ''' Provides iterable view over dictionary items. '''
+        return self._data_.items( )
+
+    def values( self ) -> __.cabc.ValuesView[ __.V ]:
+        ''' Provides iterable view over dictionary values. '''
+        return self._data_.values( )
+
+Dictionary.__doc__ = __.generate_docstring(
+    Dictionary, 'dictionary entries immutability' )
+# Register as subclass of Mapping rather than use it as mixin.
+# We directly implement, for the sake of efficiency, the methods which the
+# mixin would provide.
+__.cabc.Mapping.register( Dictionary )  # type: ignore
---
+++ sources/frigid/objects.py
@@ -0,0 +1,107 @@
+# vim: set filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+#============================================================================#
+#                                                                            #
+#  Licensed under the Apache License, Version 2.0 (the "License");           #
+#  you may not use this file except in compliance with the License.          #
+#  You may obtain a copy of the License at                                   #
+#                                                                            #
+#      http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                            #
+#  Unless required by applicable law or agreed to in writing, software       #
+#  distributed under the License is distributed on an "AS IS" BASIS,         #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#  See the License for the specific language governing permissions and       #
+#  limitations under the License.                                            #
+#                                                                            #
+#============================================================================#
+
+
+# pylint: disable=line-too-long
+''' Immutable objects.
+
+    Provides a base class and decorator for creating objects with immutable
+    attributes. Once an object is initialized, its attributes cannot be modified
+    or deleted.
+
+    The implementation uses a special dictionary type for attribute storage that
+    enforces immutability. This makes it suitable for:
+
+    * Configuration objects
+    * Value objects
+    * Immutable data containers
+    * Objects requiring attribute stability
+
+    >>> from frigid import Object
+    >>> obj = Object( x = 1, y = 2 )  # Initialize with attributes
+    >>> obj.z = 3  # Attempt to add attribute
+    Traceback (most recent call last):
+        ...
+    frigid.exceptions.AttributeImmutabilityError: Cannot assign or delete attribute 'z'.
+    >>> obj.x = 4  # Attempt modification
+    Traceback (most recent call last):
+        ...
+    frigid.exceptions.AttributeImmutabilityError: Cannot assign or delete attribute 'x'.
+'''
+# pylint: enable=line-too-long
+
+
+from . import __
+
+
+def immutable( class_: type ) -> type:
+    ''' Decorator that makes a class immutable after initialization.
+
+        Cannot be applied to classes that define their own __setattr__
+        or __delattr__ methods.
+    '''
+    for method in ( '__setattr__', '__delattr__' ):
+        if method in class_.__dict__:
+            from .exceptions import DecoratorCompatibilityError
+            raise DecoratorCompatibilityError( class_.__name__, method )
+    original_init = next(
+        base.__dict__[ '__init__' ] for base in class_.__mro__
+        if '__init__' in base.__dict__ )
+
+    def __init__(
+        self: object, *posargs: __.a.Any, **nomargs: __.a.Any
+    ) -> None:
+        attributes = super( class_, self ).__getattribute__( '__dict__' )
+        #super( class_, self ).__setattr__( '__dict__', { } )
+        behaviors: __.cabc.MutableSet[ str ] = (
+            attributes.get( '_behaviors_', set( ) ) )
+        super( class_, self ).__setattr__( '_behaviors_', behaviors )
+        original_init( self, *posargs, **nomargs )
+        super( class_, self ).__setattr__(
+            '__dict__', __.DictionaryProxy( attributes ) )
+        behaviors.add( 'immutability' )
+
+    def __delattr__( self: object, name: str ) -> None:
+        if 'immutability' in getattr( self, '_behaviors_', ( ) ):
+            from .exceptions import AttributeImmutabilityError
+            raise AttributeImmutabilityError( name )
+        super( class_, self ).__delattr__( name )
+
+    def __setattr__( self: object, name: str, value: __.a.Any ) -> None:
+        if 'immutability' in getattr( self, '_behaviors_', ( ) ):
+            from .exceptions import AttributeImmutabilityError
+            raise AttributeImmutabilityError( name )
+        super( class_, self ).__setattr__( name, value )
+
+    class_.__init__ = __init__
+    class_.__delattr__ = __delattr__
+    class_.__setattr__ = __setattr__
+    return class_
+
+
+@immutable
+class Object:
+    ''' Base class for immutable objects.
+
+        Instances become immutable after initialization.
+        Attempts to modify attributes after initialization will raise
+        AttributeImmutabilityError.
+    '''
+
+    __slots__ = ( '__dict__', '_behaviors_' )
